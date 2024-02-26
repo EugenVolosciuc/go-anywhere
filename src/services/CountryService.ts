c51c6cb7f06aa4098e19d55b89eacbb643998684
Eugen Volosciuc <volosciuc.eugen@gmail.com>
2024-02-26 13:46:02 +0200

    feat: country area compensation

--- src/services/CountryService.ts
+++ src/services/CountryService.ts
@@ -6,6 +6,19 @@ import { GeospatialService } from "./GeospatialService";
 import { StatisticsService } from "./StatisticsService";
 
 export class CountryService {
+  // For enormous countries (Russia, Canada, China, United States, Brazil, Australia) make the score not as aggresive,
+  // because these countries' central location is not the best way to calculate the proximity score
+  // Because of this, subtract a small amount from the normalized score to compensate for their big area.
+  // https://www.worldometers.info/geography/largest-countries-in-the-world/
+  static CountryAreaCompensation: Record<string, number> = {
+    RU: 0.25,
+    CA: 0.14,
+    CN: 0.14,
+    US: 0.13,
+    BR: 0.12,
+    AU: 0.1,
+  };
+
   static calculateTravelPeriodScore(
     country: Country,
     travelPeriod: TravelPeriod
@@ -35,14 +48,10 @@ export class CountryService {
       country.location.coordinates as [number, number]
     );
 
-    console.log("countryCentralLocation", countryCentralLocation);
-
     const distance = GeospatialService.calculateHaversineDistance(
       startingPoint,
       countryCentralLocation
     );
-
-    console.log("distance", distance);
 
     const normalizedScore = StatisticsService.minMaxNormalization(
       distance,
@@ -50,7 +59,9 @@ export class CountryService {
       GeospatialService.LONGEST_DISTANCE_ON_EARTH
     );
 
-    return 1 - normalizedScore;
+    const scoreCompensation = this.CountryAreaCompensation[country.alpha2] || 0;
+
+    return 1 - normalizedScore + scoreCompensation;
   }
   // calculateProximityScore
   // calculateSafetyScore
